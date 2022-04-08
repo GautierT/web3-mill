e9beeed8b7b4ebfb72111a97f1a61e1d93c5139c
Glitch (starter-cra-and-express) <none>
2022-04-08 09:13:45 +0000

    💏☃️ Checkpoint ./package.json:54995/17757 ./src/hooks/metamask.js:54995/2309 ./components/wallet/connectors.js:54995/150 ./src/components/wallet/connectors.js:54995/150 ./src/App.js:54995/1434 ./src/App.css:54995/1056 ./src/index.css:54995/732 ./src/index.js:54995/628

--- src/index.js
+++ src/index.js
@@ -4,8 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+import './index.css'
+
+import Web3 from 'web3'
+import { Web3ReactProvider } from '@web3-react/core'
+import { MetaMaskProvider } from './hooks/metamask'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+console.log('Starting....')
+
+function getLibrary(provider, connector) {
+  console.log('getLibrary')
+  return new Web3(provider)
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <MetaMaskProvider>
+        <App />
+      </MetaMaskProvider>
+    </Web3ReactProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
